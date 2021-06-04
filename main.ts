b97f732463423b282f5a757fbeb935a638e6dc82
Tarosa <tarosa.y@gmail.com>
2021-06-04 16:40:24 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,20 +5,16 @@ bluetooth.onBluetoothDisconnected(function () {
     basic.showIcon(IconNames.Heart)
 })
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(input.lightLevel())
+	
 })
-input.onButtonPressed(Button.B, function () {
-    pins.servoWritePin(AnalogPin.P1, 20)
-    basic.pause(480)
-    pins.servoWritePin(AnalogPin.P1, 147)
+bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    cmd = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
+    basic.showString(cmd)
+    bluetooth.uartWriteNumber(input.lightLevel())
 })
+let cmd = ""
 bluetooth.startUartService()
 basic.showIcon(IconNames.Happy)
 basic.forever(function () {
-    basic.pause(1000)
-    if (input.lightLevel() > 2) {
-        pins.servoWritePin(AnalogPin.P1, 20)
-        basic.pause(480)
-        pins.servoWritePin(AnalogPin.P1, 147)
-    }
+	
 })
