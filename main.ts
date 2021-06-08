2637a46b8d5dfb85998a63c9b4d68f7e5b7f633b
Tarosa <tarosa.y@gmail.com>
2021-06-08 13:28:11 +0900

    省エネにした

--- main.ts
+++ main.ts
@@ -1,20 +1,33 @@
 bluetooth.onBluetoothConnected(function () {
+    led.enable(true)
     basic.showIcon(IconNames.Pitchfork)
 })
 bluetooth.onBluetoothDisconnected(function () {
-    basic.showIcon(IconNames.Heart)
+    led.enable(false)
 })
 input.onButtonPressed(Button.A, function () {
-	
+    led.enable(true)
+    basic.showNumber(input.lightLevel())
+    basic.pause(2000)
+    led.enable(false)
 })
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     cmd = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
-    basic.showString(cmd)
     bluetooth.uartWriteNumber(input.lightLevel())
+})
+input.onButtonPressed(Button.B, function () {
+    led.enable(true)
+    basic.showNumber(input.lightLevel())
+    basic.pause(2000)
+    led.enable(false)
 })
 let cmd = ""
 bluetooth.startUartService()
+led.enable(true)
 basic.showIcon(IconNames.Happy)
+basic.pause(2000)
+led.setBrightness(2)
+led.enable(false)
 basic.forever(function () {
 	
 })
